5e9fbeb9e7a09f3e900a0e71449374822b5d29fd
john <gualterosjohn40@gmail.com>
2025-05-14 16:53:27 -0500

    initial commit

---
+++ src/mantenimiento/Mantenimiento.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mantenimiento;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Mantenimiento {
+    public static void main(String[] args) {}
+}
+
+
