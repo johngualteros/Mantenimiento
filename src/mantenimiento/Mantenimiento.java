52db2b09299fa993413c08bfd2e9bce9efbdff17
john <gualterosjohn40@gmail.com>
2025-05-14 16:57:30 -0500

    implementando primera historia

--- src/mantenimiento/Mantenimiento.java
+++ src/mantenimiento/Mantenimiento.java
@@ -9,7 +9,42 @@ import java.util.Scanner;
 
 
 public class Mantenimiento {
-    public static void main(String[] args) {}
+    static ArrayList<String> historial = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String opcion;
+        String nombre = "";
+
+
+        System.out.println("Bienvenido al sistema del grupo X");
+
+
+        do {
+            System.out.println("\n--- Menú ---");
+            System.out.println("1. Ingresar nombre y saludar");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextLine();
+
+
+            switch (opcion) {
+                case "1":
+                    System.out.print("Ingrese su nombre: ");
+                    nombre = sc.nextLine();
+                    String saludo = "Hola " + nombre + ", ¡bienvenido!";
+                    System.out.println(saludo);
+                    historial.add("Saludo: " + saludo);
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intente de nuevo.");
+            }
+        } while (!opcion.equals("4"));
+
+
+        sc.close();
+    }
+
 }
 
 
